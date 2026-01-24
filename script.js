43d18884d0b649d32adf4197b5df5f58934cb24a
ajin <3560521455@qq.com>
2026-01-24 13:29:21 +0800

    push

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const box = document.querySelector('.box');
-const imgArr = box.children;
+// const imgArr = box.children;
 
-for (let i = 0; i < imgArr.length; i++) {
-  imgArr[i].addEventListener('click', function() {
-    document.body.style.backgroundImage = `url(${imgArr[i].src})`;
-  });//反引号的作用``
-}
+// for (let i = 0; i < imgArr.length; i++) {
+//   imgArr[i].addEventListener('click', function() {
+//     document.body.style.backgroundImage = `url(${imgArr[i].src})`;
+//   });//反引号的作用``
+// }
+
+//上文方法的进阶版(委托)
+box.addEventListener('click',function(e){
+    if(e.target.nodeName==='IMG'){
+        document.body.style.backgroundImage=`url(${e.target.src})`;//注意反引号，修改对象，属性名称；
+    }
+});//行数少了，哇哦
+
+
 const likes = document.querySelectorAll('.likes');
 const likesNum = document.querySelectorAll('.likes .num');
 let arrClick = [false,false,false,false,false];
@@ -23,3 +32,10 @@ for(let j = 0;j<likes.length;j++){
         }
     });
 }
+
+
+const notice = document.querySelector('.notice');
+notice.style.display='none';
+document.addEventListener('click',function(){
+    notice.style.display='block';
+});
